2effbea049404aff2fa96b224713ef21a22ddf14
Brad Serbu <bradserbu@bradserbu.com>
2016-03-18 17:58:41 -0400

    Working on Server host component.

--- src/interfaces/rest.js
+++ src/interfaces/rest.js
@@ -20,8 +20,36 @@ class RestInterface extends Interface {
         // ** Load configuration properties
         this.host = config.host;
         this.port = config.port;
+        this.basePath = config.basePath;
 
+        // ** Create HTTP Server
+        self.api = restify.createServer({
+            name: self.basePath
+        });
 
+        this.api.use(restify.dateParser());
+        this.api.use(restify.queryParser());
+        this.api.use(restify.bodyParser({
+            maxBodySize: 0,
+            mapParams: true,
+            mapFiles: false,
+            hash: 'sha1'
+        }));
+        this.api.use(restify.gzipResponse());
+    }
+
+    start() {
+        logger.info('REST: Starting Http Listener...', {host: self.host, port: self.port});
+        this.api.listen(this.port, this.host);
+
+        super.start();
+    }
+
+    stop() {
+        logger.info('REST: Stopping Http Listener...');
+        self.api.close();
+
+        super.stop();
     }
 }
 
