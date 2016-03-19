cc0185d58d74ed03fc196b94ffe7ecd6ed161be1
Brad Serbu <bradserbu@bradserbu.com>
2016-03-18 23:36:22 -0400

    Server starting/stopping services and interface successfully.

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 'use strict';
 
+// ** Libraries
+const Server = require('../lib/Server.js');
+
 // ** Platform
 const logger = require('nodus-framework').logging.createLogger();
 const cli = require('nodus-framework').cli;
@@ -8,10 +11,24 @@ const cli = require('nodus-framework').cli;
 // ** Load CLI options and arguments
 const options = cli.options();
 const args = cli.arguments();
+const config = options.config;
 
-logger.info({
-    args: args,
-    options: options,
-    host: host,
-    port: port
+const server = new Server(args, options, config);
+
+// ** Shutdown the server on SIGINT, SIGTERM
+['SIGTERM', 'SIGINT'].forEach(signal => {
+    process.on(signal, () => {
+        logger.info(`**** ${signal} RECEIVED **** `);
+        logger.info('Shutting down server...');
+
+        server.stop();
+        process.exit();
+    });
 });
+
+// ** Load when the server is started and stopped
+server.on('started', () => logger.info('Server started.'));
+server.on('stopped', () => logger.info('Server stopped.'));
+
+// ** Start the server
+server.start();
