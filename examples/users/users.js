386a2dbf1a16a7a87f1b00ef254f1d25bc15de43
Brad Serbu <bradserbu@bradserbu.com>
2016-03-20 11:56:18 -0400

    Adding users example to store user details in a database.

---
+++ examples/users/users.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// ** Platform
+const errors = require('nodus-framework').errors;
+const logger = require('nodus-framework').logging.createLogger();
+
+module.exports = () => {
+    throw errors('NOT_IMPLEMENTED');
+};
