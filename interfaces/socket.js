c0a4325c8845b2bd326e2fa334770509ba3c88e5
Brad Serbu <bradserbu@bradserbu.com>
2016-03-22 13:46:45 -0400

    Adding a Chat Server example.

--- interfaces/socket.js
+++ interfaces/socket.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// ** Dependencies
+const http = require('http');
+const io = require('socket.io');
+
 // ** Libraries
 const Interface = require('../lib/Interface');
 
@@ -8,21 +12,41 @@ const errors = require('nodus-framework').errors;
 const logger = require('nodus-framework').logging.createLogger();
 
 class SocketInterface extends Interface {
-    constructor(args, options) {
-        super(args, options);
+    constructor(args, options, config) {
+        super(args, options, config);
 
-        throw errors('NOT_IMPLEMENTED');
+        this.host = config.host;
+        this.port = config.port;
+
+        this.server = http.createServer(req, res) => {
+            const index = ''
+        };
+
+        this.socket = io(this.server);
+
+        // ** Manage client connections
+        this.socket.on('connection', socket => {
+            logger.info('SOCKET:', socket);
+
+            socket.on('close', () => {
+                logger.info('SOCKET_CLOSED:', socket);
+            })
+        });
     }
+
     start() {
         // ** Start Web Socket Server
+        logger.info('Listening for Web Socket connections.', {host: this.host, port: this.port});
+        this.server.listen(this.host, this.port);
 
         // ** Receive a REQUEST from the WebSocket server
         // ** Send a RESPONSE to the WebSocket server
 
         // ** Broadcast events via web socket subscriptions
-        
+
         super.start();
     }
+
     stop() {
         // ** Stop Web Socket Server
         super.stop();
